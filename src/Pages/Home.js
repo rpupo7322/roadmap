001770e978346a0b32e7b07783f975258bf61a76
Rob Pupo <pupo.robert@gmail.com>
2022-08-26 13:00:07 -0400

    template to add in svg elements

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import MindMap from "react-mindmap";
-import map from "../Components/Map";
 import NavBar from "../Components/NavBar";
 
 const Home = () => {
@@ -9,6 +7,36 @@ const Home = () => {
   return (
     <div>
       <NavBar />
+
+      <svg version="1.1"
+      width="300" height="200"
+      xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="red" />
+        <circle cx="150" cy="100" r="80" fill="green" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+      </svg>
+
+      <svg>
+        <g>
+          <rect x="0" y= "0" width = '300' height = '40' fill="rgb(255,255,0)"></rect>
+          <text x="90" y= "20" fill="red" font-style="normal" font-weight="bold" font-size="17" >
+            <tspan>
+            Version Control
+            </tspan>  
+          </text>
+        </g>
+        <g>
+          <rect x="0" y= "0" width = '300' height = '40' fill="rgb(255,255,0)"></rect>
+          <text x="90" y= "20" fill="red" font-style="normal" font-weight="bold" font-size="17" >
+            <tspan>
+            Repo hosting services
+            </tspan>  
+          </text>
+        </g>
+
+      </svg>
+
 
     </div>
   );
